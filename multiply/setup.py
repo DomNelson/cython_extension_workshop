821971113cd9e84cd7b4f0460b231be13d27e718
DomNelson <nelson.dominic@gmail.com>
2018-09-13 11:42:59 -0400

    Expand documentation of build commands

--- multiply/setup.py
+++ multiply/setup.py
@@ -5,14 +5,12 @@ from Cython.Build import cythonize
 import numpy as np
 
 examples_extension = Extension(
-    name="pymultiply", # Arbitrary (?) name
+    name="pymultiply", # This is what you will call 'import' on
     sources=["pymultiply.pyx"], # Cython interface file
-    libraries=["multiply"], # Name of compiled C library
-    library_dirs=["."],
-    include_dirs=[".",
-                    np.get_include()] # If numpy is ever used
+    libraries=["multiply"], # Links to library named 'lib[name].a
+    library_dirs=["."], # Could be omitted, since everything in one dir
+    include_dirs=[np.get_include()] # Need numpy header files
 )
 setup(
-    name="PyMultiply",
     ext_modules=cythonize([examples_extension])
 )
